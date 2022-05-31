0efb4c462067fdad95211d6712036cfcf7d0a154
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2022-05-31 23:55:30 +0200

    *) implemented main page *) added new js functions for parsing and printing data from a json file

--- files/index.js
+++ files/index.js
@@ -14,11 +14,66 @@ $(document).ready(function() {
 });
 
 function toggleLoginPopup() {
-    document.getElementById("popup-1")
+    document.querySelector("#popup-1")
         .classList.toggle("active");
 }
 
 function toggleSignUpPopup() {
-    document.getElementById("popup-2")
+    document.querySelector("#popup-2")
         .classList.toggle("active");
+}
+
+function getLocation() {
+    const result = document.querySelector("#location");
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            result.innerHTML = "Geolocation is not supported by this browser.";
+        }
+
+    function showPosition(position) {
+        result.innerHTML = "Your current coordinates are: " + position.coords.latitude + " Latitude, " + position.coords.longitude + " Longitude";
+    }
+}
+
+function getSortOrder(prop) {
+    return function(a, b) {
+        if (a[prop] > b[prop]) {
+            return 1;
+        } else if (a[prop] < b[prop]) {
+            return -1;
+        }
+        return 0;
+    }
+}
+
+function jsonToArray(my_JSON_object) {
+    const obj = JSON.parse(my_JSON_object);
+    const res = [];
+
+    for(const i in obj) {
+        res.push(obj[i]);
+    }
+    return res;
+}
+
+function getResults() {
+    getLocation();
+    let request = new XMLHttpRequest();
+    request.open("GET", "../../files/testdata.json", false);
+    request.send(null);
+    let my_JSON_object = JSON.parse(request.responseText);
+
+    request.onload = function() {
+        for (let x in my_JSON_object) {
+            let listElement = document.createElement("li");
+            let itemName = document.createElement("span");
+            itemName.textContent = "Station: " + my_JSON_object[x].name + " [" + my_JSON_object[x].stationType + "], " + my_JSON_object[x].distance + "m";
+            listElement.append(itemName);
+            document.querySelector("#list-of-results").append(listElement);
+        }
+    }
+    request.open("POST", "mainpage.html", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send("x=" + my_JSON_object);
 }
