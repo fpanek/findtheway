3a9dbbaedbfc336f057a99962ef557ccd7fd7d5d
Florian Panek <florian.panek@outlook.com>
2022-06-01 20:50:16 +0200

    WIP - GET locations trial

---
+++ WebAPI/WebAPI/startServer.py
@@ -0,0 +1,37 @@
+from flask import Flask, json
+from flask import request
+import requests
+from WienerLinienAPI.getNearbyStations import saveNearbyStationsIntoFile
+from WienerLinienAPI.filterStations import stationsWithinRadius
+
+#Test Data for stations:
+stations = [{"stations": {"station1": {"name": "asdf","distance": "1","stationType": "Bus"},"station2": {"name": "bbbb","distance": "3", "stationType": "train" }, "station3": {"name": "CCCCC","distance": "6",  "stationType": "Bus"	},	"station4": {"name": "DDDDDD",	 "distance": "2","stationType": "ubahn"  } }}]
+
+stationsFile = "stations.json"
+
+api = Flask(__name__)
+
+@api.route('/stations', methods=['GET'])
+def get_companies():
+  return json.dumps(stations)
+
+@api.route('/getstations', methods=['GET'])
+def api_test():
+    long = request.args.get('long', default=00.00000)
+    lat = request.args.get('lat', default=00.00000)
+    rad = request.args.get('rad', default=500)
+    valuetoReturn = stationsWithinRadius(stationsFile, float(int(rad)/1000), long, lat)
+    return json.dumps(valuetoReturn)  #dividide by 1000 for meters instead of km
+    #valueToReturn = {"longitude": long, "latitude": lat}
+    #return json.dumps(valueToReturn)
+
+
+#Request for locations around actual position:
+#https://localhost:$PORT/location?long=45.123213&lat=12.00000&distance=500m
+
+#Request for departure times:
+#http://localhost:$PORT/departureTimes?station=XYZ
+
+if __name__ == '__main__':
+    saveNearbyStationsIntoFile(stationsFile)    #store stations locally during startup
+    api.run()
