db5e04ff7b2c4806e1b7ed957b887db42f6381bc
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2022-06-15 19:36:02 +0200

    *) node

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+      './files/index.html',
+      './files/mainpage.html',
+      './files/index.js',
+  ],
+  theme: {
+    extend: {
+        backgroundImage: {
+            'waves': "url('/files/images/wave_bottom.svg')",
+        }
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+    daisyui: {
+        styled: true,
+        themes: ["cupcake", "dark", "cmyk"],
+        base: true,
+        utils: true,
+        logs: false,
+        rtl: false,
+        prefix: "",
+        darkTheme: "dark",
+    }
+
+}
