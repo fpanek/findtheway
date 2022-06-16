7de87838e4855b92effc8cb498ee152a671aab1d
Anastasia Urusova <anastasiya.urusova@stud.fh-campuswien.ac.at>
2022-06-16 21:01:48 +0200

    *) you can now load actual stations depending on location

--- files/index.js
+++ files/index.js
@@ -1,39 +1,71 @@
 function getLocation() {
-    const result = document.querySelector("#location");
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            result.innerHTML = "Geolocation is not supported by this browser.";
-        }
-
-    function showPosition(position) {
-        result.innerHTML = "Your current coordinates are: " + position.coords.latitude + " Latitude, " + position.coords.longitude + " Longitude";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getResults);
+    } else {
+        document.querySelector("#getResultsButton").appendChild(document.createTextNode("Geolocation is not supported by this browser."));
     }
 }
 
-function getResults() {
-    getLocation();
+function getResults(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let userDistance = document.querySelector("#chosenDistance").value;
+
     let request = new XMLHttpRequest();
-    request.open("GET", "testdata.json", false);
+    request.open("GET", `http://findtheway.geokhugo.com:5000/getstations?long=${lon}&lat=${lat}&rad=${userDistance}`, false);
     request.send(null);
     let my_JSON_object = JSON.parse(request.responseText);
-    console.log(my_JSON_object);
+    reload();
 
     request.onload = function() {
-        let ul = document.createElement("ul");
-        ul.setAttribute("id", "list-of-results");
-
+        let count = 1;
         for (let x in my_JSON_object) {
-            let listElement = document.createElement("li");
-            listElement.appendChild(document.createTextNode(my_JSON_object[x].name + " [" + my_JSON_object[x].stationType + "], " + my_JSON_object[x].distance + "m"));
-            ul.appendChild(listElement);
+            for(let y in my_JSON_object[x]){
+                if(my_JSON_object[x][y]["stationName"] !== undefined) {
+                    let tr = document.createElement("tr");
+                    tr.setAttribute("class", "hover");
+
+                    let thCount = document.createElement("th");
+                    thCount.textContent = String(count);
+
+                    let stationName = document.createElement("td");
+                    let stationType = document.createElement("td");
+                    let stationDistance = document.createElement("td");
+
+                    stationDistance.textContent = (my_JSON_object[x][y]["distance"]);
+                    stationType.textContent = (my_JSON_object[x][y]["stationType"]);
+                    stationName.textContent = (my_JSON_object[x][y]["stationName"]);
+
+                    let thButton = document.createElement("th");
+                    let btn = document.createElement("button");
+                    btn.textContent = 'details';
+                    btn.setAttribute("class", "btn btn-ghost btn-xs");
+                    thButton.appendChild(btn);
+
+                    tr.append(thCount);
+                    tr.append(stationName);
+                    tr.append(stationType);
+                    tr.append(stationDistance);
+                    tr.append(thButton);
+                    document.querySelector("#stations").appendChild(tr);
+                    ++count;
+                }
+            }
         }
-        let title = document.createElement("p");
-        title.textContent = "Your results:";
-        document.querySelector("#results").appendChild(title);
-        document.querySelector("#results").appendChild(ul);
     }
+
     request.open("POST", "mainpage.html", true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.send("x=" + my_JSON_object);
+    console.log("end of index.js");
+}
+
+function reload() {
+    let stationsList = document.querySelector("#stations");
+    let child = stationsList.lastElementChild;
+    while (child) {
+        stationsList.removeChild(child);
+        child = stationsList.lastElementChild;
+    }
+    console.log("Refreshed");
 }
